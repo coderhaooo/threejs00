398bce6d5fddb4e36bc24cf19d72396d2e1a357a
ChenH <chenhao6319@163.com>
2024-03-03 16:47:08 +0800

    1. 添加动画 2. 修改一些属性

--- main.js
+++ main.js
@@ -1,29 +1,50 @@
 import './style.css'
-import {Scene, PerspectiveCamera, WebGLRenderer, BoxGeometry, MeshBasicMaterial, Mesh} from "three";
+import {Scene, PerspectiveCamera, WebGLRenderer, BoxGeometry, MeshBasicMaterial, Mesh, AxesHelper, Clock} from "three";
 
-const WIDTH = window.innerWidth;
-const HEIGHT = window.innerHeight;
-/* scene 场景 */
+const WIDTH = document.getElementById("app").clientWidth;
+const HEIGHT = document.getElementById("app").clientHeight;
+
 const scene = new Scene();
 
-/* Object 物体 */
 const geometry = new BoxGeometry(1, 1, 1);
-const material = new MeshBasicMaterial();
-const mesh = new Mesh(geometry, material);
-scene.add(mesh);
+const material = new MeshBasicMaterial({ color: 0x0000ff });
+const cube = new Mesh(geometry, material);
+scene.add(cube);
 
-/* camera 相机 */
-const camera = new PerspectiveCamera(75, WIDTH / HEIGHT, 0.1, 100)
-camera.position.set(2, 2, 2);
-camera.lookAt(0, 0, 0);
+const axes = new AxesHelper(10)
+scene.add(axes)
 
-/* render 渲染器 */
+//PerspectiveCamera() 中的 4 个参数分别为：
+//1、fov(field of view 的缩写)，可选参数，默认值为 50，指垂直方向上的角度，注意该值是度数而不是弧度
+//2、aspect，可选参数，默认值为 1，画布的宽高比(宽/高)，例如画布宽300像素，高150像素，那么意味着宽高比为 2
+//3、near，可选参数，默认值为 0.1，近平面，限制摄像机可绘制最近的距离，若小于该距离则不会绘制(相当于被裁切掉)
+//4、far，可选参数，默认值为 2000，远平面，限制摄像机可绘制最远的距离，若超出该距离则不会绘制(相当于被裁切掉)
+//以上 4 个参数在一起，构成了一个 “视椎”
+const camera = new PerspectiveCamera(75, WIDTH/HEIGHT, 0.1, 1000)
+
+//之前初始化透视镜头时，设置的近平面为 0.1，远平面为 1000
+//因此 camera.position.z 的值一定要在 0.1 - 1000 的范围内，超出这个范围则画面不会被渲染
+camera.position.set(0,0,5);
+camera.lookAt(scene.position);
+
 const render = new WebGLRenderer();
+render.setClearColor(0xffffff);
 render.setSize(WIDTH, HEIGHT);
-render.render(scene, camera);
-
-// 将渲染器绑定到指定的DOM元素中
 document.getElementById("app").appendChild(render.domElement);
+
+const clock = new Clock();
+function animate() {
+    const time = clock.getElapsedTime(); // time的值基本一定，解决高刷屏上动画速度过快的问题
+    cube.position.x = 2 * Math.sin(time);
+    cube.position.y = 2 * Math.cos(time);
+    cube.rotation.z = time;
+    render.render(scene, camera);
+    requestAnimationFrame(animate)
+}
+
+animate();
+
+
 
 
 
